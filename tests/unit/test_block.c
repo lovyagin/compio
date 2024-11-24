0903c3f327a636d84587f376a6ac79cd3aa927e1
Pand <officialpand@gmail.com>
2024-11-24 12:14:43 +0300

    compio_find_block_by_offset testing

--- tests/unit/test_block.c
+++ tests/unit/test_block.c
@@ -97,6 +97,34 @@ void test_find_block(void) {
     compio_free_block_container(container);
 }
 
+void test_find_block_by_offset(void) {
+    compio_block_container* container = compio_create_block_container(8192);
+    compio_block* block1 = compio_create_block(1024, false, NULL);
+    compio_block* block2 = compio_create_block(2048, false, NULL);
+    compio_add_block(container, block1);
+    compio_add_block(container, block2);
+
+    size_t internal_offset = 0;
+
+    // Test finding block by offset within the first block
+    compio_block* found_block = compio_find_block_by_offset(container, 512, &internal_offset);
+    CU_ASSERT_PTR_NOT_NULL(found_block);
+    CU_ASSERT_PTR_EQUAL(found_block, block1);
+    CU_ASSERT_EQUAL(internal_offset, 512);
+
+    // Test finding block by offset within the second block
+    found_block = compio_find_block_by_offset(container, 1500, &internal_offset);
+    CU_ASSERT_PTR_NOT_NULL(found_block);
+    CU_ASSERT_PTR_EQUAL(found_block, block2);
+    CU_ASSERT_EQUAL(internal_offset, 476);
+
+    // Test finding block by offset that is out of range
+    found_block = compio_find_block_by_offset(container, 5000, &internal_offset);
+    CU_ASSERT_PTR_NULL(found_block);
+
+    compio_free_block_container(container);
+}
+
 // Function to add block tests to the test registry
 void add_block_tests() {
     CU_pSuite block_suite = CU_add_suite("Block Tests", NULL, NULL);
@@ -107,5 +135,6 @@ void add_block_tests() {
         CU_add_test(block_suite, "test_add_block", test_add_block);
         CU_add_test(block_suite, "test_remove_block", test_remove_block);
         CU_add_test(block_suite, "test_find_block", test_find_block);
+        CU_add_test(block_suite, "test_find_block_by_offset", test_find_block_by_offset); // Add the new test here
     }
 }
