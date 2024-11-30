1d7e1a732fbb531adbaf58a2de5a0079983aae8f
mozhaa <mozhay2005@gmail.com>
2024-11-30 15:00:21 +0300

    compressor struct + mock dummy compressor

--- include/compression.h
+++ include/compression.h
@@ -5,4 +5,27 @@
 #ifndef COMPIO_COMPRESSION_H
 #define COMPIO_COMPRESSION_H
 
+#include <stdio.h>
+
+
+/**
+ * @brief Compressor interface
+ * 
+ * @param compress Compress data
+ * @param decompress Deompress data
+ */
+typedef struct compio_compressor {
+    int (*compress)(void* dst, const void* src, size_t size);
+    int (*decompress)(void* dst, const void* src, size_t size);
+} compio_compressor;
+
+
+/**
+ * @brief Dummy compressor, that does nothing
+ * 
+ * @param result Pointer to resulting compressor 
+ */
+void build_dummy_compressor(compio_compressor* result);
+
+
 #endif //COMPIO_COMPRESSION_H
--- src/compression.c
+++ src/compression.c
@@ -1,3 +1,23 @@
 //
 // Реализация абстракции для выбора алгоритмов сжатия
 //
+
+#include <string.h>
+
+#include "compression.h"
+
+
+int dummy_compress(void* dst, const void* src, size_t size) {
+    memcpy(dst, src, size);
+}
+
+
+int dummy_decompress(void* dst, const void* src, size_t size) {
+    memcpy(dst, src, size);
+}
+
+
+void build_dummy_compressor(compio_compressor* result) {
+    result->compress = dummy_compress;
+    result->decompress = dummy_decompress;
+}
