103074b00fad270d3b64e06146e0489c69acaa31
Nikita <6u8654u4@gmail.com>
2025-02-23 11:53:15 +0300

    created storageBlock class

--- include/storage/storage.hpp
+++ include/storage/storage.hpp
@@ -1,4 +1,8 @@
 #ifndef STORAGE_H
 #define STORAGE_H
 
+class Storage {
+public:
+    Storage() = default;
+};
 #endif //STORAGE_H
---
+++ include/storage/storage_block.hpp
@@ -0,0 +1,9 @@
+#ifndef STORAGE_BLOCK_HPP
+#define STORAGE_BLOCK_HPP
+
+class StorageBlock {
+public:
+    StorageBlock() = default;
+};
+
+#endif //STORAGE_BLOCK_HPP
