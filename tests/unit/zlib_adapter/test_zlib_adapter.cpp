ee1250500e1139a094dc9df7044cb6851f47dd0e
Nikita <6u8654u4@gmail.com>
2025-02-24 15:47:04 +0300

    deleted debug prints

--- tests/unit/zlib_adapter/test_zlib_adapter.cpp
+++ tests/unit/zlib_adapter/test_zlib_adapter.cpp
@@ -26,40 +26,25 @@ void testChangeData() {
     auto mockBTreeP = std::make_shared<MockBTree>();
     ZLibAdapter adapter{mockBTreeP};
 
-    std::cout << "[test]: created adapter" << std::endl;
     compio::tree_key key = {123, 0};
     compio::tree_val val = {new StorageBlock{compressedSize, compressedData}, 5};
     mockBTreeP->nodes.emplace_back(MockBTree::KeyValuePair{key, val});
     mockBTreeP->insert_segment(123, {0, 10});
 
-    std::cout << "[test]: created node" << std::endl;
-
     int result = compress(compressedData, &compressedSize, testData, sizeof(testData));
     CU_ASSERT_EQUAL(result, Z_OK);
-
-    std::cout << "[test]: COMPRESSED OK, compressed size: " << compressedSize << std::endl;
 
     unsigned char newData[] = {0x06, 0x07};
     adapter.change_data(123, 1, 2, newData);
 
-    std::cout << "[test]: changed data" << std::endl;
     CU_ASSERT_EQUAL(mockBTreeP->nodes.size(), 1);
     auto& updatedNode = mockBTreeP->nodes[0];
     CU_ASSERT_EQUAL(updatedNode.val.size, 5);
-
-    std::cout << "[test]: data check" << std::endl;
 
     unsigned char decompressedData[1024];
     ulong decompressedSize = sizeof(decompressedData);
     result = uncompress(decompressedData, &decompressedSize, updatedNode.val.addr->compressedData, updatedNode.val.addr->compressedDataSize);
     CU_ASSERT_EQUAL(result, Z_OK);
-
-    std::cout << "[test]: unpacked data" << std::endl;
-
-    std::cout <<  "[test]: decompressedData[1]: " << static_cast<int>(decompressedData[1]) << std::endl;
-    std::cout <<  "[test]: decompressedData[2]: " << static_cast<int>(decompressedData[2]) << std::endl;
     CU_ASSERT_EQUAL(decompressedData[1], 0x06);
     CU_ASSERT_EQUAL(decompressedData[2], 0x07);
-
-    std::cout << "[test]: data check" << std::endl;
 }
