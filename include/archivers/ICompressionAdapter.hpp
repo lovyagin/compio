2da5d373fe59a8d86276afe5c9485dda08e977a4
Nikita <6u8654u4@gmail.com>
2025-02-25 17:28:39 +0300

    added change one block function to compression adapter

--- include/archivers/ICompressionAdapter.hpp
+++ include/archivers/ICompressionAdapter.hpp
@@ -15,6 +15,7 @@ public:
     virtual void change_data(uint64_t hash, uint64_t startPos, uint64_t size, void* data) = 0;
 private:
     std::unique_ptr<compio::btree> _btreeP;
+    virtual void change_one_block_data(uint64_t hash, uint64_t startPos, uint64_t size, void* data) = 0;
 };
 
 #endif //ICOMPRESSION_ADAPTER_H
--- include/archivers/zlib_adapter.hpp
+++ include/archivers/zlib_adapter.hpp
@@ -16,6 +16,7 @@ public:
     void change_data(uint64_t hash, uint64_t startPos, uint64_t size, void* data) override;
 private:
     std::shared_ptr<compio::IBTree> _IbtreeP; // TODO: CREATE INTERFACE FOR TREE
+    void change_one_block_data(uint64_t hash, uint64_t startPos, uint64_t size, void* data) override {}
 };
 
 #endif //ZLIB_ADAPTER_H
