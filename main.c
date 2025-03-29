48d6020c4d3629ee12c56ef758b89e7591a6f34f
mozhaa <mozhay2005@gmail.com>
2025-03-29 13:12:42 +0300

    example main.c

--- main.c
+++ main.c
@@ -1,34 +1,60 @@
 #include "compio.h"
 
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     compio_config config;
     compio_build_default_config(&config);
     config.b_tree_degree = 3;
     config.block_size = 8;
-
-    compio_archive* archive = compio_open_archive("test.archive", "w+", &config);
-    compio_file* file = compio_open_file("fileA", archive);
     
-    char data[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-    compio_write(data, sizeof(data), file);
+    if (!strcmp(argv[1], "read")) {
+        compio_archive* archive = compio_open_archive(argv[2], "r", &config);
+        
+        if (!archive) {
+            printf("failed to open archive, errno=%d\n", errno);
+        }
 
-    compio_close_file(file);
-    compio_close_archive(archive);
+        compio_file* file = compio_open_file("fileA", archive);
+    
+        if (!file) {
+            printf("failed to open file, errno=%d\n", errno);
+        }
+        
+        char data[17];
+        char test[sizeof(data)];
+        uint64_t bytes = compio_read(test, sizeof(data), file);
+    
+        printf("read %d bytes\n", bytes);
 
-    archive = compio_open_archive("test.archive", "r", &config);
-    file = compio_open_file("fileA", archive);
+        for (int i = 0; i < bytes; ++i)
+            printf("%llu, ", test[i]);
+        printf("\n");
+    
+        compio_close_file(file);
+        compio_close_archive(archive);
+    } else {
+        compio_archive* archive = compio_open_archive(argv[2], "w+", &config);
+        
+        if (!archive) {
+            printf("failed to open archive, errno=%d\n", errno);
+        }
 
-    char test[sizeof(data)];
-    compio_read(test, sizeof(data), file);
+        compio_file* file = compio_open_file("fileA", archive);
+    
+        if (!file) {
+            printf("failed to open file, errno=%d\n", errno);
+        }
 
-    for (int i = 0; i < sizeof(data); ++i)
-        printf("%d, ", test[i]);
-    printf("\n");
+        char data[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+        uint64_t bytes = compio_write(data, sizeof(data), file);
 
-    compio_close_file(file);
-    compio_close_archive(archive);
+        printf("written %llu bytes\n", bytes);
+        
+        compio_close_file(file);
+        compio_close_archive(archive);
+    }
 
     return 0;
 }
