b1f509469014d50a96a6e2cf4e7a78eff54dee27
Nikita Zuev <6u8654u4@gmail.com>
2025-04-05 19:06:34 +0300

    fix(utils): fixed conficts

--- src/utils.cpp
+++ src/utils.cpp
@@ -60,15 +60,9 @@ tree_key get_key(const char* fname, uint64_t pos) {
     auto hashed_fname = hash.sha256_final();
     uint64_t hash_tail;
 
-<<<<<<< HEAD
-    // Исправление: преобразование итератора в указатель
-    memcpy(&hash_tail, hashed_fname.data(), sizeof(uint64_t)); // Если есть .data()
-    // ИЛИ
-    memcpy(&hash_tail, &(*hashed_fname.begin()), sizeof(uint64_t)); // Для итераторов
-=======
+
     memcpy(&hash_tail, hashed_fname.data(), sizeof(uint64_t));
     memcpy(&hash_tail, &(*hashed_fname.begin()), sizeof(uint64_t));
->>>>>>> develop
 
     return {hash_tail, pos};
 }
