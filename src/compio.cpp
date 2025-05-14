bc977c304dd705c8aa96d9f64c626e8c0c37c43d
mozhaa <mozhay2005@gmail.com>
2025-05-14 15:57:20 +0300

    set max cache size in default config

--- src/compio.cpp
+++ src/compio.cpp
@@ -18,7 +18,7 @@ void compio_build_default_config(compio_config* result) {
     result->fill_holes_with_zeros = true;
     result->swap_endianness = false;
     result->block_size = 4096;
-    result->cache_size = -1;
+    result->cache_size = 128;
     result->allocation_strategy = COMPIO_ALLOC_FIRST_FIT;
     result->fragmentation_threshold = 30;
 }
