c7d6a585b916b18c2ce6b124cef0943c8e1aefaa
mozhaa <mozhay2005@gmail.com>
2025-05-19 16:51:24 +0300

    fragmentation graph util

--- tools/plot_csv.py
+++ tools/plot_csv.py
@@ -11,10 +11,13 @@ def plot_csv_columns(args: argparse.Namespace) -> None:
     for column in data.columns:
         plt.plot(data[column], label=column)
 
-    plt.xlabel("n_blocks")
-    plt.ylabel("filesize")
-    plt.legend()
+    plt.xlabel(args.xlabel)
+    plt.ylabel(args.ylabel)
+    if args.legend:
+        plt.legend()
     plt.grid(True)
+    if args.xlog:
+        plt.xscale("log")
     plt.savefig(args.outfile)
     plt.close()
 
@@ -22,6 +25,10 @@ def plot_csv_columns(args: argparse.Namespace) -> None:
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("infile", type=str, help="path to CSV file")
+    parser.add_argument("--xlabel", type=str, default="")
+    parser.add_argument("--ylabel", type=str, default="")
+    parser.add_argument("--xlog", action="store_true")
+    parser.add_argument("--legend", action="store_true")
     parser.add_argument("outfile", type=str, help="path to output image")
     return parser.parse_args()
 
