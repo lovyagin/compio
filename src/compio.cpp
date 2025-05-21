a489040ceea5cd723fd00ce2d5e3e1a8e90b4f66
mozhaa <mozhay2005@gmail.com>
2025-05-21 12:52:25 +0300

    optimization: don't fill holes with zeros

--- src/compio.cpp
+++ src/compio.cpp
@@ -18,7 +18,7 @@ extern "C" {
 void compio_build_default_config(compio_config* result) {
     result->b_tree_degree = 16;
     compio_build_zlib_compressor(&result->compressor);
-    result->fill_holes_with_zeros = true;
+    result->fill_holes_with_zeros = false;
     result->block_size = 4096;
     result->cache_size = 128;
     result->block_cache_size = 16;
