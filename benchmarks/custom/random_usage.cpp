7c61b995103fc9b2fa657795ba1199a42ada11a9
mozhaa <mozhay2005@gmail.com>
2025-05-24 20:26:03 +0300

    random usage executable (for profiling)

---
+++ benchmarks/custom/random_usage.cpp
@@ -0,0 +1,79 @@
+#include "compio.h"
+#include "sample_data.hpp"
+#include "util.hpp"
+
+int main() {
+    compio_config config;
+    compio_archive* archive;
+    compio_file* file;
+
+    std::string fn = get_temporary_filename();
+
+    compio_build_default_config(&config);
+
+    std::size_t file_size = 1 << 14;
+    std::size_t n_operations = 1 << 8;
+    std::size_t n_repetitions = 1;
+
+    std::minstd_rand rng;
+    std::uniform_int_distribution<int> d_op(0, 3);
+    std::uniform_int_distribution<int> d_pos(0, file_size - 2);
+
+    for (int k = 0; k < n_repetitions; ++k) {
+        rng.seed(k);
+
+        std::vector<unsigned char> buffer(file_size);
+        int cursor = 0;
+        int current_fsize = 0;
+
+        archive = compio_open_archive(fn.c_str(), "w+", &config);
+        file = compio_open_file("A", archive);
+
+        for (int i = 0; i < n_operations; ++i) {
+            switch (d_op(rng)) {
+            case 0: {
+                cursor = d_pos(rng);
+                compio_seek(file, cursor, COMP_SEEK_SET);
+                break;
+            }
+            case 1: {
+                compio_tell(file);
+                break;
+            }
+            case 2: {
+                if (cursor < current_fsize) {
+                    std::size_t max_size = current_fsize - cursor;
+                    std::uniform_int_distribution<int> d_size(1, max_size);
+                    int size = d_size(rng);
+
+                    compio_read(buffer.data(), size, file); 
+                    cursor += size;
+                    break;
+                }
+            }
+            case 3: {
+                if (cursor < file_size) {
+                    std::size_t max_size = std::min<uint64_t>(file_size - cursor, sizeof(html_data));
+                    std::uniform_int_distribution<int> d_size(1, max_size);
+                    int size = d_size(rng);
+
+                    std::uniform_int_distribution<int> d_start(0, sizeof(html_data) - size);
+                    int start = d_start(rng);
+                    
+                    compio_write(html_data + start, size, file);
+                    cursor += size;
+                    current_fsize = std::max(current_fsize, cursor);
+                    break;
+                }
+            }
+            }
+        }
+
+        compio_close_file(file);
+        compio_close_archive(archive);
+    }
+
+    remove(fn.c_str());
+
+    return 0;
+}
