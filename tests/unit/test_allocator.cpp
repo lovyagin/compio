63735e9f58bc80d7bb47144b0534f5c6cf873767
dkorbelainen <officialpand@gmail.com>
2025-09-28 10:40:19 +0300

    fix!: correct include paths in test CMakeLists.txt

--- tests/unit/test_allocator.cpp
+++ tests/unit/test_allocator.cpp
@@ -2,9 +2,9 @@
 #include <gmock/gmock.h>
 #include <vector>
 #include <algorithm>
-#include "../include/allocator.hpp"
-#include "../include/compio_file.hpp"
-#include "../include/utils.hpp"
+#include "allocator.hpp"
+#include "compio_file.hpp"
+#include "utils.hpp"
 #include "random"
 
 using namespace compio;
